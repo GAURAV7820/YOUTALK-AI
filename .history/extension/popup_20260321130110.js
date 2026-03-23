d266e160018fef8b8f6879ebde38c1968b84cce5
GAURAV7820 <thakurgaurav7820@gmail.com>
2026-03-23 09:30:53 +0530

    Remove sensitive history files

--- .history/extension/popup_20260321130110.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("askBtn").addEventListener("click", () => {
-    let question = document.getElementById("question").value;
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { question: question });
-    });
-});
